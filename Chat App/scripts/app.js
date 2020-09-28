aeeeb4a8f3523e1024d0e675f523d178993bfeb0
Mehmet Eren Olgun <erenolgun1907@gmail.com>
2020-09-28 16:26:04 +0300

    changing the chat room functionality added

--- Chat App/scripts/app.js
+++ Chat App/scripts/app.js
@@ -3,6 +3,7 @@ const chatList = document.querySelector('.chat-list');
 const newChatForm = document.querySelector('.new-chat');
 const newNameForm = document.querySelector('.new-name');
 const updateMessage = document.querySelector('.update-mssg');
+const rooms = document.querySelector('.chat-rooms');
 
 // add a new chat
 newChatForm.addEventListener('submit', e => {
@@ -30,7 +31,17 @@ newNameForm.addEventListener('submit', e => {
     setTimeout(() => updateMessage.innerText = '', 3000)
 });
 
-// chech local storage for a name
+// update the chat room
+rooms.addEventListener('click', e => {
+    if(e.target.tagName === 'BUTTON') {
+        chatUI.clear();
+    }
+
+    chatroom.updateRoom(e.target.getAttribute('id'));
+    chatroom.getChats(chat => chatUI.render(chat));
+});
+
+// check local storage for a name
 const username = localStorage.username ? localStorage.username : 'anonymous';
 
 // class instances
