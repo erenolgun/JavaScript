383d899c12446dd7fdff392389460ec22dc47225
Mehmet Eren Olgun <erenolgun1907@gmail.com>
2020-09-28 15:00:34 +0300

    updateRoom() and updateName() added

--- Chat App/scripts/chat.js
+++ Chat App/scripts/chat.js
@@ -8,6 +8,7 @@ class Chatroom {
         this.room = room;
         this.username = username;
         this.chats = db.collection('chats');
+        this.unsub;
     }
 
     async addChat(message) {
@@ -26,7 +27,7 @@ class Chatroom {
     }
 
     getChats(callback) {
-        this.chats
+        this.unsub = this.chats
             .where('room', '==', this.room)
             .orderBy('created_at')
             .onSnapshot(snapshot => {
@@ -38,6 +39,18 @@ class Chatroom {
                 })
             })
     }
+
+    updateName(username) {
+        this.username = username;
+    }
+
+    updateRoom(room) {
+        this.room = room;
+        console.log('room is updated');
+        if(this.unsub) {
+            this.unsub();
+        }
+    }
 }
 
 const chatroom = new Chatroom('general', 'shaun');
@@ -45,3 +58,12 @@ const chatroom = new Chatroom('general', 'shaun');
 chatroom.getChats((data) => {
     console.log(data);
 })
+
+setTimeout(() => {
+    chatroom.updateRoom('gaming');
+    chatroom.updateName('yoshi')
+    chatroom.getChats((data) => {
+        console.log(data);
+    });
+    chatroom.addChat('hello')
+}, 3000);
